248472b9fca9b6ac6923865fbc8b110fde39c276
Mvmo <entchenmomoyt@gmail.com>
2018-05-11 08:26:24 +0200

    first version of index class

--- database/index/index.go
+++ database/index/index.go
@@ -1,15 +1,41 @@
 package index
 
-import "net"
+import (
+	"net"
+	"strings"
+	"strconv"
+	"fmt"
+)
 
 type Index struct {
 	Map map[string]interface{}
 }
 
-func New(objId string) (*Index, error) {
-	conn, err := net.Dial("tcp", "22522")
+var (
+	defaultPort = 22522
+	connection net.Conn
+)
+
+func CreateConnection(host string, port int) bool {
+	var err error
+	connection, err = net.Dial("tcp", strings.Join([]string{host, strconv.Itoa(port)}, ":"))
+	if err != nil {
+		return false
+	}
+	return true
 }
 
-func (i *Index) Get() {
+func GetIndex(indexId string) (*Index) {
+	if connection == nil {
+		if !CreateConnection("localhost", defaultPort) {
+			return nil
+		}
+	}
 
+	connection.Write([]byte("get id=" + indexId + "\n"))
+	buff := make([]byte, 1024)
+	n, _ := connection.Read(buff)
+	fmt.Println(buff[:n])
+
+	return nil
 }
--- main.go
+++ main.go
@@ -1,9 +1,6 @@
 package main
 
-import (
-	"net"
-)
-
+import "mvmo.eu/tacodb/database/index"
 
 //port : 22522
 
@@ -11,9 +8,6 @@ func main() {
 	/*if index, err := index.New("1"); err == nil {
 		fmt.Println(index.Map["name"])
 	}*/
-	if conn, err := net.Dial("tcp", ":22522"); err == nil {
-		conn.Write([]byte("get id=1\n"))
-		select {
-		}
-	}
+	index.GetIndex("1")
+
 }
